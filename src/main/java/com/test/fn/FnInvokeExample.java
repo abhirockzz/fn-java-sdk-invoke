7bc823c3d5e7c32aa94341da72ce9f373ab4b47b
abhishek <>
2019-02-08 08:58:42 +0530

    refactored helper methods into a separate class

--- src/main/java/com/test/fn/FnInvokeExample.java
+++ src/main/java/com/test/fn/FnInvokeExample.java
@@ -2,22 +2,10 @@ package com.test.fn;
 
 import com.google.common.base.Supplier;
 import com.oracle.bmc.Region;
-
 import com.oracle.bmc.auth.SimpleAuthenticationDetailsProvider;
 import com.oracle.bmc.functions.FunctionsClient;
-import com.oracle.bmc.functions.requests.GetFunctionRequest;
 import com.oracle.bmc.functions.requests.InvokeFunctionRequest;
-import com.oracle.bmc.functions.requests.ListApplicationsRequest;
-import com.oracle.bmc.functions.requests.ListFunctionsRequest;
-import com.oracle.bmc.functions.responses.GetFunctionResponse;
 import com.oracle.bmc.functions.responses.InvokeFunctionResponse;
-import com.oracle.bmc.functions.responses.ListApplicationsResponse;
-import com.oracle.bmc.functions.responses.ListFunctionsResponse;
-import com.oracle.bmc.http.DefaultConfigurator;
-import com.oracle.bmc.identity.IdentityClient;
-import com.oracle.bmc.identity.model.Compartment;
-import com.oracle.bmc.identity.requests.ListCompartmentsRequest;
-import com.oracle.bmc.identity.responses.ListCompartmentsResponse;
 import com.oracle.bmc.util.StreamUtils;
 
 import java.io.File;
@@ -26,19 +14,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.security.KeyManagementException;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.SSLSession;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import javax.ws.rs.client.ClientBuilder;
 
 import org.apache.commons.io.IOUtils;
-import org.glassfish.jersey.SslConfigurator;
 
 public class FnInvokeExample {
 
@@ -46,9 +23,10 @@ public class FnInvokeExample {
      * Oracle Functions service endpoint (us-phoenix-1 corresponds to phoenix
      * region where the service is available)
      */
-    private static final String FAAS_ENDPOINT = "https://functions.us-phoenix-1.oraclecloud.com/";
+    public static final String FAAS_ENDPOINT = "https://functions.us-phoenix-1.oraclecloud.com/";
     static String tenantOCID = null;
     private SimpleAuthenticationDetailsProvider authDetails;
+    private Util ociUtil;
 
     static String ERR_MSG = "Usage: java -jar <jar-name>.jar <compartment name> <app name> <function name> <(optional) function invoke payload>";
 
@@ -74,7 +52,6 @@ public class FnInvokeExample {
         String appName = args[1];
         String funcName = args[2];
         String invokePayload = args.length == 4 ? args[3] : "";
-        
 
         System.out.println("Invoking function " + funcName + " from app " + appName + " in compartment " + compartmentName + " from tenancy " + tenantOCID);
         test.invokeFunction(funcName, appName, compartmentName, invokePayload);
@@ -109,6 +86,8 @@ public class FnInvokeExample {
                 .region(Region.US_PHOENIX_1)
                 .build();
 
+        this.ociUtil = new Util(authDetails);
+
     }
 
     /**
@@ -123,15 +102,15 @@ public class FnInvokeExample {
     public void invokeFunction(String functionName, String appName, String compartmentName, String payload) throws Exception {
 
         //get the App OCID first
-        String appOCID = getAppOCID(appName, compartmentName, tenantOCID);
+        String appOCID = ociUtil.getAppOCID(appName, compartmentName, tenantOCID);
 
         //find the function OCID
-        String functionId = getFunctionOCID(appOCID, functionName);
+        String functionId = ociUtil.getFunctionOCID(appOCID, functionName);
 
         //find the function invoke endpoint
-        String invokeEndpoint = getFunctionInvokeEndpoint(functionId);
+        String invokeEndpoint = ociUtil.getFunctionInvokeEndpoint(functionId);
 
-        try (FunctionsClient fnClient = new FunctionsClient(authDetails, null, new TrustAllConfigurator())) {
+        try (FunctionsClient fnClient = new FunctionsClient(authDetails, null, new Util.TrustAllConfigurator())) {
 
             System.out.println("Invoking function endpoint - " + invokeEndpoint + " with payload " + payload);
 
@@ -172,166 +151,4 @@ public class FnInvokeExample {
 
     }
 
-    /**
-     * Returns application OCID for an application
-     *
-     * @param appName Name of the application whose OCID is required
-     * @param compartmentName Name of the compartment where the Oracle Functions
-     * service is configured
-     * @param tenantOCID OCID of the tenancy i.e. root compartment
-     *
-     * @return Application OCID
-     *
-     * @throws Exception
-     */
-    public String getAppOCID(String appName, String compartmentName, String tenantOCID) throws Exception {
-
-        //start by finding the compartment OCID from the name
-        String compOCID = getCompartmentOCID(compartmentName, tenantOCID);
-
-        System.out.println("Finding OCID for App " + appName);
-        try (FunctionsClient fnClient = new FunctionsClient(authDetails, null, new TrustAllConfigurator())) {
-            fnClient.setEndpoint(FAAS_ENDPOINT);
-
-            //find the application in a specific compartment
-            ListApplicationsRequest req = ListApplicationsRequest.builder()
-                    .displayName(appName)
-                    .compartmentId(compOCID)
-                    .build();
-            ListApplicationsResponse resp = fnClient.listApplications(req);
-
-            if (resp.getItems().isEmpty()) {
-                throw new Exception("Could not find App with  name " + appName + " in compartment " + compartmentName);
-
-            }
-            String appOCID = resp.getItems().get(0).getId();
-
-            System.out.println("Application OCID " + appOCID);
-
-            return appOCID;
-
-        }
-    }
-
-    /**
-     * Gets compartment OCID
-     *
-     * @param compartmentName
-     * @param tenantOCID
-     * @return compartment OCID
-     * @throws Exception
-     */
-    public String getCompartmentOCID(String compartmentName, String tenantOCID) throws Exception {
-        System.out.println("Finding OCID for Compartment " + compartmentName);
-
-        ListCompartmentsRequest lcr = ListCompartmentsRequest.builder().compartmentId(tenantOCID).build();
-        ListCompartmentsResponse listCompartmentsResponse = null;
-        try (IdentityClient idc = new IdentityClient(authDetails)) {
-            listCompartmentsResponse = idc.listCompartments(lcr);
-        }
-        String compOCID = null;
-        for (Compartment comp : listCompartmentsResponse.getItems()) {
-            if (comp.getName().equals(compartmentName)) {
-                compOCID = comp.getId();
-                break;
-            }
-        }
-
-        if (compOCID == null) {
-            throw new Exception("Could not find compartment with  name " + compartmentName + " in tenancy " + tenantOCID);
-        }
-        System.out.println("Compartment OCID " + compOCID);
-
-        return compOCID;
-    }
-
-    /**
-     * Get function OCID
-     * 
-     * @param appOCID
-     * @param functionName
-     * @return function OCID
-     * @throws Exception 
-     */
-    public String getFunctionOCID(String appOCID, String functionName) throws Exception {
-
-        System.out.println("Finding OCID for function " + functionName);
-
-        //search for the function in the app
-        ListFunctionsRequest lfr = ListFunctionsRequest.builder().applicationId(appOCID).displayName(functionName).build();
-
-        try (FunctionsClient fnClient = new FunctionsClient(authDetails, null, new TrustAllConfigurator())) {
-            fnClient.setEndpoint(FAAS_ENDPOINT);
-
-            ListFunctionsResponse lfresp = fnClient.listFunctions(lfr);
-
-            if (lfresp.getItems().isEmpty()) {
-                throw new Exception("Could not find function with  name " + functionName + " for application " + appOCID);
-
-            }
-            String functionId = lfresp.getItems().get(0).getId();
-            System.out.println("Function OCID " + functionId);
-
-            return functionId;
-        }
-    }
-
-    /**
-     * Get function invoke endpoint
-     * 
-     * @param functionId
-     * @return function invoke endpoint
-     */
-    public String getFunctionInvokeEndpoint(String functionId) {
-        try (FunctionsClient fnClient = new FunctionsClient(authDetails, null, new TrustAllConfigurator())) {
-            fnClient.setEndpoint(FAAS_ENDPOINT);
-
-            System.out.println("Finding invoke endpoint for function " + functionId);
-
-            //get function details
-            GetFunctionRequest gfr = GetFunctionRequest.builder().functionId(functionId).build();
-
-            GetFunctionResponse function = fnClient.getFunction(gfr);
-            String invokeEndpoint = function.getFunction().getInvokeEndpoint();
-            return invokeEndpoint;
-        }
-    }
-
-    public static class TrustAllConfigurator extends DefaultConfigurator {
-
-        @Override
-        protected void setSslContext(ClientBuilder builder) {
-            SSLContext sslContext
-                    = SslConfigurator.newInstance(true).securityProtocol("TLSv1.2").createSSLContext();
-            builder.sslContext(sslContext);
-            try {
-                sslContext.init(
-                        null,
-                        new TrustManager[]{
-                            new X509TrustManager() {
-                        public X509Certificate[] getAcceptedIssuers() {
-                            return null;
-                        }
-
-                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                        }
-
-                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                        }
-                    }
-                        },
-                        new SecureRandom());
-            } catch (KeyManagementException e) {
-                System.err.println(e);
-            }
-
-            builder.hostnameVerifier(
-                    new HostnameVerifier() {
-                @Override
-                public boolean verify(String hostname, SSLSession session) {
-                    return true;
-                }
-            });
-        }
-    }
 }
