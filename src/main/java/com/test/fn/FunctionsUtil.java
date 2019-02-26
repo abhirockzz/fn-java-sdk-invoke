68e7837fa55e451a68a04f39b40a2781ed9e31cc
Shaun Smith <shaun.smith@oracle.com>
2019-02-26 17:35:17 -0500

    Initial revisions

--- src/main/java/com/test/fn/FunctionsUtil.java
+++ src/main/java/com/test/fn/FunctionsUtil.java
@@ -1,32 +1,46 @@
+/***
+ * @author abhirockzz
+ * @author shaunsmith
+ */
+
 package com.test.fn;
 
 import com.google.common.base.Supplier;
 import com.oracle.bmc.Region;
 import com.oracle.bmc.auth.SimpleAuthenticationDetailsProvider;
+import com.oracle.bmc.functions.FunctionsInvokeClient;
 import com.oracle.bmc.functions.FunctionsManagementClient;
+import com.oracle.bmc.functions.model.ApplicationSummary;
 import com.oracle.bmc.functions.model.FunctionSummary;
+import com.oracle.bmc.functions.requests.InvokeFunctionRequest;
 import com.oracle.bmc.functions.requests.ListApplicationsRequest;
 import com.oracle.bmc.functions.requests.ListFunctionsRequest;
+import com.oracle.bmc.functions.responses.InvokeFunctionResponse;
 import com.oracle.bmc.functions.responses.ListApplicationsResponse;
 import com.oracle.bmc.functions.responses.ListFunctionsResponse;
 import com.oracle.bmc.identity.IdentityClient;
 import com.oracle.bmc.identity.model.Compartment;
 import com.oracle.bmc.identity.requests.ListCompartmentsRequest;
 import com.oracle.bmc.identity.responses.ListCompartmentsResponse;
+import com.oracle.bmc.util.StreamUtils;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
 
 /**
  * Contains helper methods
  */
-public class FunctionsUtil {
+public class FunctionsUtil implements AutoCloseable {
 
     private String tenantOCID = null;
     private final FunctionsManagementClient fnMgtClient;
     private final IdentityClient identityClient;
+	private FunctionsInvokeClient fnInvokeClient;
 
     /**
      * Initializes FunctionsManagementClient and IdentityClient
@@ -60,10 +74,54 @@ public class FunctionsUtil {
         this.fnMgtClient = new FunctionsManagementClient(authDetails);
         this.fnMgtClient.setRegion(Region.US_PHOENIX_1); //Oracle Functions currently only in phoenix
         this.identityClient = new IdentityClient(authDetails);
+		this.fnInvokeClient = new FunctionsInvokeClient(authDetails);
+
     }
 
-    /**
-     * Gets Function information 
+ 
+	/**
+	 * Invokes a function with the given payload
+	 * 
+	 * @param functionId
+	 * @param invokeEndpoint
+	 * @param payload
+	 * @throws Exception
+	 */
+	public void invokeFunction(FunctionSummary function, String payload) throws Exception {
+		try {
+			System.err.println("Invoking function endpoint - " + function.getInvokeEndpoint() + " with payload " + payload);
+
+			// the client needs to use the function invoke endpoint
+			this.fnInvokeClient.setEndpoint(function.getInvokeEndpoint());
+
+			/**
+			 * This is an example of sending a JSON payload as an input to the function -
+			 * valid for "fn init --runtime ..." generated boilerplate functions for python,
+			 * node, go, and ruby. The expected result is {"message":"Hello foobar"}
+			 *
+			 * For a Java boilerplate function, you can simply pass a string (not JSON)
+			 * e.g., foobar as the input and expect Hello foobar! as the response
+			 *
+			 * see README for note on how to send binary payload
+			 */
+			InvokeFunctionRequest ifr = InvokeFunctionRequest.builder().functionId(function.getId())
+					.invokeFunctionBody(StreamUtils.createByteArrayInputStream(payload.getBytes())).build();
+
+			// actual function invocation
+			InvokeFunctionResponse resp = fnInvokeClient.invokeFunction(ifr);
+
+			// parse the response - this example below simply reads a String response.
+			String respString = IOUtils.toString(resp.getInputStream(), StandardCharsets.UTF_8);
+			System.err.print("Response from function - " + respString + "\n");
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			fnInvokeClient.close();
+		}
+	}
+
+	/**
+     * Gets Function information.  This is an expensive operation and the results should be cached. 
      * 
      * @param compartmentName
      * @param appName
@@ -72,56 +130,84 @@ public class FunctionsUtil {
      * @throws Exception 
      */
     public FunctionSummary getFunction(String compartmentName, String appName, String functionName) throws Exception {
+        Compartment compartment = getCompartment(compartmentName);
+        return getFunction(compartment, appName, functionName);
+    }
 
-        System.out.println("Finding OCID for function " + functionName);
 
-        String appOCID = getAppOCID(appName, compartmentName);
+	/**
+     * Gets Function information.  This is an expensive operation and the results should be cached. 
+     * 
+     * @param compartment
+     * @param appName
+     * @param functionName
+     * @return
+     * @throws Exception 
+     */
+	public FunctionSummary getFunction(Compartment compartment, String appName, String functionName) throws Exception {
+		ApplicationSummary application = getApplication(compartment, appName);
+        return getFunction(application, functionName);
+	}
 
-        //search for the function in the app
-        ListFunctionsRequest lfr = ListFunctionsRequest.builder().applicationId(appOCID).displayName(functionName).build();
 
-        //fnMgtClient.setEndpoint(FnInvokeExample.FAAS_ENDPOINT);
+	/**
+     * Gets Function information.  This is an expensive operation and the results should be cached. 
+     * 
+     * @param application
+	 * @param functionName
+     * @return
+     * @throws Exception 
+     */
+	public FunctionSummary getFunction(ApplicationSummary application, String functionName) throws Exception {
+        ListFunctionsRequest lfr = ListFunctionsRequest.builder().applicationId(application.getId()).displayName(functionName).build();
+
         ListFunctionsResponse lfresp = fnMgtClient.listFunctions(lfr);
 
         if (lfresp.getItems().isEmpty()) {
-            throw new Exception("Could not find function with  name " + functionName + " for application " + appOCID);
-
+            throw new Exception("Could not find function with name " + functionName + " in application " + application.getDisplayName());
         }
+        
         FunctionSummary function = lfresp.getItems().get(0);
-        System.out.println("Found Function with OCID " + function.getId());
+        System.err.println("Found Function with OCID " + function.getId());
 
         return function;
-    }
-
+	}
+ 
     /**
-     * Gets application OCID
+     * Gets application info
      * 
      * @param appName
      * @param compartmentName
      * @return
      * @throws Exception 
      */
-    private String getAppOCID(String appName, String compartmentName) throws Exception {
+    public ApplicationSummary getApplication(String compartmentName, String appName) throws Exception {
+    	Compartment compartment = getCompartment(compartmentName);
+    	return getApplication(compartment, appName);
+    }
+    	
+    /**
+     * Gets application info
+     * @param appName
+     * @param compartmentName
+     * 
+     * @return
+     * @throws Exception 
+     */
+    public ApplicationSummary getApplication(Compartment compartment, String appName) throws Exception {
 
-        //start by finding the compartment OCID from the name
-        String compOCID = getCompartmentOCID(compartmentName);
-        System.out.println("Finding OCID for App " + appName);
-
-        //find the application in a specific compartment
+         //find the application in a specific compartment
         ListApplicationsRequest req = ListApplicationsRequest.builder()
                 .displayName(appName)
-                .compartmentId(compOCID)
+                .compartmentId(compartment.getId())
                 .build();
         ListApplicationsResponse resp = fnMgtClient.listApplications(req);
 
         if (resp.getItems().isEmpty()) {
-            throw new Exception("Could not find App with  name " + appName + " in compartment " + compartmentName);
-
+            throw new Exception("Could not find application with name " + appName + " in compartment " + compartment.getName());
         }
-        String appOCID = resp.getItems().get(0).getId();        
-        System.out.println("Application OCID " + appOCID);
-
-        return appOCID;
+        ApplicationSummary application = resp.getItems().get(0);       
+        return application;
     }
 
     /**
@@ -131,38 +217,26 @@ public class FunctionsUtil {
      * @return
      * @throws Exception 
      */
-    private String getCompartmentOCID(String compartmentName) throws Exception {
-        System.out.println("Finding OCID for Compartment " + compartmentName);
-        String compOCID = null;
+    public Compartment getCompartment(String compartmentName) throws Exception {
         ListCompartmentsResponse listCompartmentsResponse = null;
-        try {
-            ListCompartmentsRequest lcr = ListCompartmentsRequest.builder()
-                    .compartmentId(tenantOCID)
-                    .accessLevel(ListCompartmentsRequest.AccessLevel.Accessible)
-                    .compartmentIdInSubtree(Boolean.TRUE)
-                    .build();
+        ListCompartmentsRequest lcr = ListCompartmentsRequest.builder()
+                .compartmentId(tenantOCID)
+                .accessLevel(ListCompartmentsRequest.AccessLevel.Accessible)
+                .compartmentIdInSubtree(Boolean.TRUE)
+                .build();
 
-            listCompartmentsResponse = identityClient.listCompartments(lcr);
+        listCompartmentsResponse = identityClient.listCompartments(lcr);
 
-            for (Compartment comp : listCompartmentsResponse.getItems()) {
-                if (comp.getName().equals(compartmentName)) {
-                    compOCID = comp.getId();
-                    break;
-                }
+        for (Compartment comp : listCompartmentsResponse.getItems()) {
+            if (comp.getName().equals(compartmentName)) {
+                return comp;
             }
-
-            if (compOCID == null) {
-                throw new Exception("Could not find compartment with  name " + compartmentName + " in tenancy " + tenantOCID);
-            }
-            System.out.println("Compartment OCID " + compOCID);
-        } catch (Exception e) {
-            throw e;
         }
-        return compOCID;
+        throw new Exception("Could not find compartment with name " + compartmentName + " in tenancy " + tenantOCID);
     }
 
     /**
-     * closes FunctionsManagementClient and IdentityClient instances
+     * close client instances
      */
     public void close() {
         if (fnMgtClient != null) {
@@ -171,6 +245,10 @@ public class FunctionsUtil {
 
         if (identityClient != null) {
             identityClient.close();
+        }
+
+        if (fnInvokeClient != null) {
+            fnInvokeClient.close();
         }
     }
 
--- src/main/java/com/test/fn/MainClient.java
+++ src/main/java/com/test/fn/MainClient.java
@@ -1,51 +1,53 @@
+/***
+ * @author abhirockzz
+ * @author shaunsmith
+ */
+
 package com.test.fn;
 
+import com.oracle.bmc.functions.model.ApplicationSummary;
 import com.oracle.bmc.functions.model.FunctionSummary;
+import com.oracle.bmc.identity.model.Compartment;
 
 public class MainClient {
 
-    static String ERR_MSG = "Usage: java -jar <jar-name>.jar <compartment name> <app name> <function name> <(optional) function invoke payload>";
+	static String ERR_MSG = "Usage: java -jar <jar-name>.jar <compartment name> <app name> <function name> <(optional) function invoke payload>";
 
-    public static void main(String[] args) throws Exception {
 
-        if (args.length < 3) {
-            throw new Exception(ERR_MSG);
-        }
+	public static void main(String[] args) throws Exception {
 
-        String tenantOCID = System.getenv("TENANT_OCID");
-        String userId = System.getenv("USER_OCID");
-        String fingerprint = System.getenv("PUBLIC_KEY_FINGERPRINT");
-        String privateKeyFile = System.getenv("PRIVATE_KEY_LOCATION");
-        String passphrase = System.getenv("PASSPHRASE");
+		if (args.length < 3) {
+			throw new Exception(ERR_MSG);
+		}
 
-        if (tenantOCID == null || userId == null || fingerprint == null || privateKeyFile == null /*|| passphrase == null*/) {
-            throw new Exception("Please ensure you have set the mandatory environment variables - TENANT_OCID, USER_OCID, PUBLIC_KEY_FINGERPRINT, PRIVATE_KEY_LOCATION");
-        }
+		String tenantOCID = System.getenv("TENANT_OCID");
+		String userId = System.getenv("USER_OCID");
+		String fingerprint = System.getenv("PUBLIC_KEY_FINGERPRINT");
+		String privateKeyFile = System.getenv("PRIVATE_KEY_LOCATION");
+		String passphrase = System.getenv("PASSPHRASE");
 
-        String compartmentName = args[0];
-        String appName = args[1];
-        String funcName = args[2];
-        String invokePayload = args.length == 4 ? args[3] : "";
+		if (tenantOCID == null || userId == null || fingerprint == null
+				|| privateKeyFile == null /* || passphrase == null */) {
+			throw new Exception(
+					"Please ensure you have set the mandatory environment variables - TENANT_OCID, USER_OCID, PUBLIC_KEY_FINGERPRINT, PRIVATE_KEY_LOCATION");
+		}
 
-        FunctionsUtil util = null;
-        try {
-            util = new FunctionsUtil(tenantOCID, userId, fingerprint, privateKeyFile, passphrase);
-            FnInvokeExample test = new FnInvokeExample(tenantOCID, userId, fingerprint, privateKeyFile, passphrase);
+		String compartmentName = args[0];
+		String appName = args[1];
+		String funcName = args[2];
+		String invokePayload = args.length == 4 ? args[3] : "";
 
-            FunctionSummary function = util.getFunction(compartmentName, appName, funcName);
-            String functionId = function.getId();
-            String invokeEndpoint = function.getInvokeEndpoint();
-            System.out.println("Invoking function " + funcName + " from app " + appName + " in compartment " + compartmentName + " from tenancy " + tenantOCID);
+		try (
+			FunctionsUtil functionsUtil = new FunctionsUtil(tenantOCID, userId, fingerprint, privateKeyFile, passphrase);
+		){	
+			Compartment compartment = functionsUtil.getCompartment(compartmentName);
+			ApplicationSummary application = functionsUtil.getApplication(compartment, appName);
+			FunctionSummary function = functionsUtil.getFunction(application, funcName);
+			System.err.println("Invoking function " + funcName + " from app " + appName + " in compartment "
+					+ compartmentName + " from tenancy " + tenantOCID);
+			functionsUtil.invokeFunction(function, invokePayload);
+		} 
 
-            test.invokeFunction(functionId, invokeEndpoint, invokePayload);
-        } catch (Exception e) {
-            throw e;
-        } finally {
-            if (util != null) {
-                util.close();
-            }
-        }
-
-    }
+	}
 
 }
